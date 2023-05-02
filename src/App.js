b0f68fa3d2a1e7e249cae50efac134fb052fec32
Mutshinye <Mutshinyen@gmail.com>
2023-05-02 16:53:02 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import logo from "./logo.png";
+import Dictionary from "./Dictionary";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <header className="App-header">
+          <a href="https://www.shecodes.io/" target="_blank" rel="noreferrer">
+            <img src={logo} className="logo" alt="SheCodes Logo" />
+          </a>
+        </header>
+        <Dictionary defaultKeyword="sunset" />
+        <footer>
+          This project was coded by Mutshinye Nethengwe
+          and is{" "}
+          <a
+            href="https://github.com/Mukona21/Dictionary-Project.git"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://shecodes-dictionary.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
+}
+
+export default App;
